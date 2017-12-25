93ddedc91dafe5d31e86242ffa81e84ab4a0cad4
Pravat Bhusal <pravat.bhusal@gmail.com>
2017-12-25 11:25:46 -0600

    Purpose of serve.py comment

--- serve.py
+++ serve.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 
+# Runs the bot using python 2.7.x +
+
 from sys import exit
 from config.config import config
 import lib.bot as bot
-
-# Twitch Plays
-# Inpsired by http://twitch.tv/twitchplayspokemon
-# Written by Aidan Thomson - <aidraj0 at gmail dot com>
 
 try:
     bot.Bot().run()
